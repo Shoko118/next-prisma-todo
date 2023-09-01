385cb5ee463ba12a31ca1b8027e1ba44134c5177
Shoko118 <secast118@gmail.com>
2023-09-01 17:00:05 +0800

    :white_check_mark: Server actions done
    Features: add, delete, edit, and edit

---
+++ app/api/todos/route.ts
@@ -0,0 +1,29 @@
+import prisma from "@/app/db";
+import { revalidateTag } from "next/cache";
+
+export async function getTodos() {
+  "use server";
+  return prisma.todo.findMany();
+}
+
+export async function toggleTodo(id: string, checked: boolean) {
+  "use server";
+  await prisma.todo.update({ where: { id }, data: { isCompleted: checked } });
+}
+
+export async function deleteTodo(id: string) {
+  "use server";
+  await prisma.todo.delete({
+    where: { id: id },
+  });
+  revalidateTag("todo");
+}
+
+export async function editTodo(id: string, newTitle: string) {
+  "use server";
+  await prisma.todo.update({
+    where: { id: id },
+    data: { title: newTitle },
+  });
+  revalidateTag("todo");
+}
---
+++ components/TodoItem.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useState } from "react";
+
+type TodoItemProps = {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+  toggleTodo: (id: string, checked: boolean) => void;
+  deleteTodo: (id: string) => void;
+  editTodo: (id: string, newTitle: string) => void;
+};
+
+export default function TodoItem({ id, title, isCompleted, toggleTodo, deleteTodo, editTodo }: TodoItemProps) {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [newTitle, setNewTitle] = useState<string>(title);
+
+  return (
+    <div className="max-w-5xl mx-auto mt-4">
+      <input
+        type="checkbox"
+        defaultChecked={isCompleted}
+        onChange={(e) => toggleTodo(id, e.target.checked)}
+        className="cursor-pointer peer"
+      />
+
+      {isEditing ? (
+        <>
+          <input value={newTitle} onChange={(e) => setNewTitle(e.target.value)} />
+          <button
+            onClick={() => {
+              editTodo(id, newTitle);
+              setIsEditing(false);
+            }}
+          >
+            Done
+          </button>
+        </>
+      ) : (
+        <>
+          <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">
+            {title}
+          </label>
+          <button className="border-2 border-blue-500 px-3 py-2 ml-2" onClick={() => setIsEditing(true)}>
+            Edit
+          </button>
+        </>
+      )}
+
+      <button className="border-2 border-red-500 px-3 py-2 ml-2" onClick={() => deleteTodo(id)}>
+        Delete
+      </button>
+
+      <style jsx>{`
+        input {
+          color: black; /* Ensure the color remains visible when focused */
+        }
+      `}</style>
+    </div>
+  );
+}
